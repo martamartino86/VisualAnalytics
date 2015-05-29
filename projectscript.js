d36ee7901bfbe4b4cbe49ec606a769de51e46ce7
Marta <martamartino86@gmail.com>
2015-05-30 01:06:31 +0200

    Stuff 30052015
    GII - colour modified;
    HEALTH - fixed stuff;
    radio button;

--- projectscript.js
+++ projectscript.js
@@ -1,7 +1,61 @@
-var map;
+var map = null;
 var colormap;
 var d1 = {}, d2 = {};
+var dsv = d3.dsv(";", "text/plain");
 
+function show_gii() {
+	console.log("show gii");
+	dsv("gii_index.csv", function(data){
+		colormap = d3.scale.linear()
+						.domain([0, 1000])
+						.range(["#FFE8F6", "#AD3D7E"]);
+		for (d in data) {
+            a = data[d].iso3;									// ISO3
+            b = parseFloat(data[d].GII2013.replace(',','.'));
+            //console.log(colormap(Math.round(1000*b)));
+            if (b == -1.0)
+            	d1[a] = "#A1A1A1"
+            else
+            	d1[a] = colormap(Math.round(1000*b));				// dizionario 1: ISO3 -> colore
+            d2[a] = {fillKey: a}									// dizionario 2: ISO3 -> fillkey: ISO3
+		}
+		d1.defaultFill = '#A1A1A1';
+		console.log(d1)
+		console.log(d2)
+		if (map == null) {
+			console.log("mumble");
+			map = new Datamap({
+			element: document.getElementById('container'),
+			dataType: 'csv',
+			fills: d1,
+			data: d2,
+			projection: "mercator",
+			geographyConfig: {
+				borderColor: '#CCCCCC'
+			}
+			});
+			// SETTA EVENTI MAPPA
+			map.svg.selectAll('.datamaps-subunit').on('click',function(geography){
+				console.log(geography);
+				// map.options.setProjection(function(element){
+				// 	var projection = d3.geo.equirectangular()
+				//     	.center([23, -3])
+				//         .rotate([4.4, 0])
+				//         .scale(400)
+				//         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+				//     var path = d3.geo.path()
+				//         .projection(projection);
+				// });
+			});
+
+		}
+		else
+		{
+			map.updateChoropleth(d1);
+			console.log("modificato");
+		}
+	});
+}
 $("document").ready(function(){
 	// CREAZIONE CANVAS SVG
 	/*
@@ -9,7 +63,6 @@ $("document").ready(function(){
 		.attr("width", 800)
 		.attr("height", 600)
 		.attr("viewBox", "0 0 800 600");
-
 	// RETTANGOLO DI VISUALIZZAZIONE
 	var mainRect = svg.append("rect")
 	   .attr("x",0)
@@ -22,43 +75,10 @@ $("document").ready(function(){
 	*/
 	var width = document.getElementById('container').offsetWidth;
 	var height = document.getElementById('container').offsetHeight;
-	var dsv = d3.dsv(";", "text/plain");
 	// MAPPA INIZIALE: GII
-	dsv("gii_index.csv", function(data){
-		colormap = d3.scale.linear()
-						.domain([0, 1000])
-						.range(["#E0FAFF", "#0099FF"]);
-		for (d in data) {
-            a = data[d].iso3;
-            b = parseFloat(data[d].GII2013.replace(',','.'));
-            //console.log(colormap(Math.round(1000*b)));
-            d1[a] = colormap(Math.round(1000*b));
-            d2[a] = {fillKey: a}
-		}
-		map = new Datamap({
-			element: document.getElementById('container'),
-			dataType: 'csv',
-			defaultFill: '#FF0000',
-			fills: d1,
-			data: d2,
-			projection: "mercator",
-			geographyConfig: {
-				borderColor: '#CCCCCC'
-			}
-		});
-		/*
-		fills: {
-			'ITA__': '#ff0000'
-		},
-		data: {
-			'ITA': {fillKey: 'ITA__'}
-		}
-		*/		
-	});
-
-
-	$("#health_btn").click(function(){
-		d1 = {};
+	show_gii();
+	$("input:radio[value='gii']").change(show_gii);
+	$("input:radio[value='health']").change(function(){
 		// implementare enter() dataset con dati health
 		dsv("health_index.csv", function(data){
 			var max_dataset = d3.max(data, function(d){return parseFloat(d.HLT2013);});
@@ -67,38 +87,36 @@ $("document").ready(function(){
 				if (q == -1) return max_dataset+1;
 				else return q;
 			});
-			console.log("min: "+min_dataset+" max: "+max_dataset);
+			//console.log("min: "+min_dataset+" max: "+max_dataset);
 			colormap = d3.scale.log()
 							.domain([300, 400, 500, 510, 520, 530, 540, 550, 560, 600, 700, max_dataset*1000])
 							.range(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]);
 			for (d in data) {
 	            a = data[d].iso3;					// Paese
 	            b = parseFloat(data[d].HLT2013);	// valore health 2013
-	            d1[a] = colormap(Math.round(1000*b));
-	         //    if (b == -1)
-	         //    	d1[a] = "CCCCCC";
-		        // else if (min_dataset <= b && b < 450)
-		        // 	d1[a] = "dark blue"
-		        // else if (450 <= b && b < 460)
-		        // 	d1[a] = "blue"
-		        // else if (460 <= b && b < 500)
-		        // 	d1[a] = "violet"
-		        // else if (500 <= b && b < 510)
-		        // 	d1[a] = "dark red"
-		        // else if (510 <= b && b < 520)
-		        // 	d1[a] = "red"
-		        // else if (520 <= b && b < 530)
-		        // 	d1[a] = "light red"
-		        // else if ()
+	            if (b == -1.0)
+            		d1[a] = "#A1A1A1"
+	            else
+	            	d1[a] = colormap(Math.round(1000*b));
 			}
 			map.updateChoropleth(d1);
 		})
+		// zoom and pan
+		var zoom = d3.behavior.zoom()
+   			.on("zoom",function() {
+		        g.attr("transform","translate("+ 
+		        	d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+		        g.selectAll("path")  
+		            .attr("d", path.projection(projection)); 
+		svg.call(zoom);
+  });
 
+svg.call(zoom)
 	});
-	$("#empow_btn").click(function(){
+	$("input:radio[value='empowerment']").change(function(){
 		// implementare enter() dataset con dati empowerment
 	});
-	$("#lfpr_btn").click(function(){
+	$("input:radio[value='labourforce']").change(function(){
 		// implementare enter() dataset con dati labour force participation rate
 	});
 		/*
@@ -111,12 +129,10 @@ $("document").ready(function(){
 			.attr("stroke", "black")
 			.append("text")
 			.text(function (d) {return d.iso3;});
-
-	
 	for(var prop in gii_index) {
     if(gii_index.hasOwnProperty(prop)) {
     	console.log(gii_index[prop]);
-        if(gii_index[prop] === "iso3") {
+        if(gii_index[pro-p] === "iso3") {
         	alert (gii_index[prop]);
         }
     }
